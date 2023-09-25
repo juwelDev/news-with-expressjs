f4fcaa7013ec5140e263d791a510e58175ea4918
SalehAhmed19 <saleh.ahmed.mahin@gmail.com>
2023-09-25 14:38:46 +0600

    fetch data from local file

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
+const path = require("path");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  const userUrl = path.join(__dirname, "../public/data/data.json");
+
+  fs.readFile(userUrl, "utf-8", function (err, data) {
+    if (err) {
+      console.error({ err: err });
+      return res.status(500).send({ message: "Error reading json file" });
+    }
+
+    const userData = JSON.parse(data);
+
+    res.status(200).send(userData);
+  });
 });
 
 module.exports = router;
