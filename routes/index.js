15782741958c304085f7de590187806513696eff
Karim Chowdhury <karimchowdhury45@gmail.com>
2023-09-30 10:33:10 +0600

    nodemon added

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ExpressJS' });
+  var a = "Hello expressJS";
+  res.render('index', { title: a });
 });
 
 module.exports = router;
